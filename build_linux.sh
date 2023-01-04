be207057592d1748ccda87db9f8ca9b018302ff9
Colleen <cdnickerson05@gmail.com>
2023-01-04 19:56:47 -0400

    Initial source commit

---
+++ build_linux.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Make sure CXX environment variable is set.
+if [ -z "$CXX" ]
+then
+    echo "error: CXX environment variable is not set. Please set this to your preferred C++ compiler."
+    exit
+fi
+
+# Introduction
+echo "Building GalaMake [linux]..."
+
+# Setting up directories
+echo "Creating directories..."
+mkdir -p bin/linux
+
+# Compiling
+echo "Compiling..."
+${CXX} -O3 -o bin/linux/galamake src/*.cpp -Iinclude -Llib/linux -lxdt --std=c++17
---
+++ src/Main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+#include <nlohmann/json.hpp>
+#include <libxdt.hpp>
+
+int main(int argc, char **argv) {
+    return 0;
+}
